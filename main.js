e7b898e549ab9b745e83953e8a2361525081450d
lwcsharp <lwcsharp@gmail.com>
2025-04-18 18:48:28 +0200

    recfactored navbar

---
+++ main.js
@@ -0,0 +1,14 @@
+import { loadNavbar } from "./navigation/navbar.js";
+
+console.log("Initializing app...");
+const initApp = async () => {
+  console.log("Initializing app...");
+  try {
+    await loadNavbar();
+    console.log("Navbar loaded successfully");
+  } catch (error) {
+    console.error("Failed to load navbar:", error);
+  }
+};
+
+document.addEventListener('DOMContentLoaded', initApp);
---
+++ navigation/navbar.js
@@ -0,0 +1,9 @@
+export async function loadNavbar() {
+  try {
+    const respone = await fetch('../navigation/navbar.html');
+    const html = await respone.text();
+    document.getElementById('navbar-container').innerHTML = html
+  } catch (error) {
+    console.error('Error Loading navbar: ', error);
+  }
+}
