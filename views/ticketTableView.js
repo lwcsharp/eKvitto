14f5d5628796929bc2830754fb2dbfa741de2bfa
lwcsharp <lwcsharp@gmail.com>
2025-08-19 18:32:41 +0200

    refactored tickettableview.js

--- views/ticketTableView.js
+++ views/ticketTableView.js
@@ -4,23 +4,24 @@ const messages = {
   error: 'Error loading data'
 };
 
-// Hjälpfunktion för statusmeddelanden
+function createTableRow(ticket) {
+  const row = document.createElement('tr');
+  row.innerHTML = `
+    <td>${ticket.date}</td>
+    <td>${ticket.specification}</td>
+    <td>${ticket.customer_name}</td>
+    <td>${ticket.total_price} kr</td>
+    <td>${ticket.ex_vat} kr</td>
+    <td>${ticket.vat} kr</td>
+  `;
+  return row;
+}
+
 export function showStatus(tbody, type) {
   tbody.innerHTML = `<tr><td colspan="6" class="loading-message">${messages[type]}</td></tr>`;
 }
 
 export function displayTickets(tbody, tickets) {
   tbody.innerHTML = '';
-  tickets.forEach((ticket) => {
-    const row = document.createElement('tr');
-    row.innerHTML = `
-      <td>${ticket.date}</td>
-      <td>${ticket.specification}</td>
-      <td>${ticket.customer_name}</td>
-      <td>${ticket.total_price} kr</td>
-      <td>${ticket.ex_vat} kr</td>
-      <td>${ticket.vat} kr</td>
-    `;
-    tbody.appendChild(row);
-  });
+  tickets.forEach(ticket => tbody.appendChild(createTableRow(ticket)));
 }
